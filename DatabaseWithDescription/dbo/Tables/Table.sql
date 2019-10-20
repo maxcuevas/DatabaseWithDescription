1dda605ca8293b0d078fa45f80e6214ef69ea450
Max Cuevas <maximilianocuevas777@gmail.com>
2019-10-19 20:17:12 -0700

    First commit

---
+++ DatabaseWithDescription/dbo/Tables/Table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Table] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [name] VARCHAR (50) NULL ,
+    [age]  INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
+GO
+
+
+
